1085627f57e984575d718bb72b24fcf83e98b837
mkdesign <me@mkdesign.ir>
2014-02-18 22:30:04 +0330

    start to work on functions

--- scripts.js
+++ scripts.js
@@ -1,18 +1,3 @@
-window.onload= function(){
-	var pushBtn = document.getElementById('push-btn');
-	var popBtn = document.getElementById('pop-btn');
-	var stack = new Array();
-	pushBtn.onclick = function(){
-
-		var str=document.getElementById('str').value;
-
-		stack.unshift(str);
-		alert(stack);
-
-	}
-	popBtn.onclick = function(){
-		stack.shift();
-		alert(stack);
-	}
-
+window.onload= function	() {
+	
 }
