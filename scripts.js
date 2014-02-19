3d3ff35472f9ee38fe1f242caf4314af722c0da2
mkdesign <me@mkdesign.ir>
2014-02-19 11:22:32 +0330

    toggle finished

--- scripts.js
+++ scripts.js
@@ -4,9 +4,16 @@ window.onload= function	() {
 	var iosToggle= document.getElementsByTagName('div');
 
 function toggle (k){
-	iosToggle.item(k).getElementsByClassName('toggle').item(0).style.float='right';
-	iosToggle.item(k).getElementsByClassName('state').item(0).style.marginLeft='0';
-	iosToggle.item(k).getElementsByClassName('state').item(0).style.marginRight='10px';
+	if(iosToggle.item(k).getElementsByClassName('toggle').item(0).style.float=='left'){
+		iosToggle.item(k).getElementsByClassName('toggle').item(0).style.float='right';
+		iosToggle.item(k).getElementsByClassName('state').item(0).style.marginLeft='0';
+		iosToggle.item(k).getElementsByClassName('state').item(0).style.marginRight='10px';
+	}else{
+		iosToggle.item(k).getElementsByClassName('toggle').item(0).style.float='left';
+		iosToggle.item(k).getElementsByClassName('state').item(0).style.marginRight='0';
+		iosToggle.item(k).getElementsByClassName('state').item(0).style.marginLeft='10px';
+	}
+
 }
 
 	for(i=0;i<iosToggle.length;i++){
