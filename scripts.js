ab22ea76a8aa8a0e2de06f1682df4e4ec3c29ca1
mkdesign <me@mkdesign.ir>
2014-02-19 12:18:29 +0330

    state condition

--- scripts.js
+++ scripts.js
@@ -8,10 +8,12 @@ function toggle (k){
 		iosToggle.item(k).getElementsByClassName('toggle').item(0).style.float='right';
 		iosToggle.item(k).getElementsByClassName('state').item(0).style.marginLeft='0';
 		iosToggle.item(k).getElementsByClassName('state').item(0).style.marginRight='10px';
+		iosToggle.item(k).getElementsByClassName('state').item(0).innerHTML='off';
 	}else{
 		iosToggle.item(k).getElementsByClassName('toggle').item(0).style.float='left';
 		iosToggle.item(k).getElementsByClassName('state').item(0).style.marginRight='0';
 		iosToggle.item(k).getElementsByClassName('state').item(0).style.marginLeft='10px';
+		iosToggle.item(k).getElementsByClassName('state').item(0).innerHTML='on';
 	}
 
 }
